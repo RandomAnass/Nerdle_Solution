8f3b8f108dcccab8ada9f7c4642b0e9e335a1111
RandomAnass <Anasss.haut.2020@gmail.com>
2022-02-08 11:39:20 +0100

    Create Data_script.py

---
+++ Data_script.py
@@ -0,0 +1,43 @@
+from multiprocessing import Pool, Manager, cpu_count
+from functools import partial
+import re
+import pickle
+
+
+def process_value(data_set_N, i_1):
+    print(i_1)
+    for i_2 in list(range(10))+list("+-*/")+["=="]:
+        for i_3 in list(range(10))+list("+-*/")+["=="]:
+            for i_4 in list(range(10))+list("+-*/")+["=="]:
+                for i_5 in list(range(10))+list("+-*/")+["=="]:
+                    for i_6 in list(range(10))+list("+-*/")+["=="]:
+                        for i_7 in list(range(10))+list("+-*/")+["=="]:
+                            for i_8 in list(range(10)):
+                                try:
+                                    value = str(i_1)+str(i_2)+str(i_3)+str(i_4)+str(i_5)+str(i_6)+str(i_7)+str(i_8)
+                                    if '//' in value:
+                                        continue
+                                    valuev = re.sub(r'\b0+(?!\b)', '', value)
+                                    evaluation = eval(valuev)
+                                    if type(evaluation) == type(True) and evaluation:
+                                        data_set_N.append(value)
+                                except:
+                                    continue
+
+if __name__ == '__main__':
+    with Manager() as manager:
+        data_set_N = manager.list()
+        # The iterable is the i_1 list:
+        i_1_list = list(range(10))+list("+-")
+        POOL_SIZE = min(cpu_count(), len(i_1_list))
+        pool = Pool(POOL_SIZE)
+        pool.map(partial(process_value, data_set_N), i_1_list)
+        pool.close()
+        pool.join()
+        data_set_N=list(data_set_N)
+        #print(len(data_set_N))
+        data_set_N2 = [equa.replace("==", "=") for equa in data_set_N]
+        filename_N = 'Nerdle_data'
+        outfile_N = open(filename_N,'wb')
+        pickle.dump(data_set_N2,outfile_N)
+        outfile_N.close()
